5e324ac3aaf3d79ade0d8f66cc74fec0debae16e
zhengyueling1993 <hxq4940@163.com>
2016-04-04 12:00:05 +0800

    WebClient.java

---
+++ WebClient.java
@@ -0,0 +1,27 @@
+/**
+ * Created by yuelingzheng on 16/4/4.
+ */
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class WebClient {
+    public String getContent(URL url) {
+        StringBuffer content = new StringBuffer();
+        try {
+            HttpURLConnection connection = (HttpURLConnection)
+                    url.openConnection();
+            connection.setDoInput(true);
+            InputStream is = connection.getInputStream();
+            byte[] buffer = new byte[2048];
+            int count;
+            while (-1 != (count = is.read(buffer))) {
+                content.append(new String(buffer, 0, count));
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return content.toString();
+    }
+}
